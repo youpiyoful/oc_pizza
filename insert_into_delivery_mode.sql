0f49d42f1224a3ffd57e5991dba63d1559856bde
youpiyoful <yoanfornari@gmail.com>
2020-11-12 15:51:43 +0100

    :sparkles: insert data into delivery_mode table

---
+++ insert_into_delivery_mode.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.delivery_mode (delivery_name) VALUES ('Livraison');
+INSERT INTO public.delivery_mode (delivery_name) VALUES ('A emporter');
+INSERT INTO public.delivery_mode (delivery_name) VALUES ('Sur place');
