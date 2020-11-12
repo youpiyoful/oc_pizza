0121ee0ff5cfb4f88a81fe9bf4a6ca2b4fa79306
youpiyoful <yoanfornari@gmail.com>
2020-11-12 16:07:55 +0100

    :sparkles: insert data into status table

---
+++ insert_into_status.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.status (status_name) VALUES ('en cours de livraison');
+INSERT INTO public.status (status_name) VALUES ('récupérée');
+INSERT INTO public.status (status_name) VALUES ('en attente de préparation');
+INSERT INTO public.status (status_name) VALUES ('en attente de livraison');
+INSERT INTO public.status (status_name) VALUES ('livrée');
