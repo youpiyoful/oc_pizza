e6431432108a256dcfe91d5eee2f5be2f3d1ecb9
youpiyoful <yoanfornari@gmail.com>
2020-11-12 15:48:31 +0100

    :sparkles: insert data in payment_choice table

---
+++ insert_paiment_choice.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.payment_choice (paiment_name) VALUES ('CB');
+INSERT INTO public.payment_choice (paiment_name) VALUES ('Espèces');
+INSERT INTO public.payment_choice (paiment_name) VALUES ('Paypal');
