da46718925f58f08073b53f5525114f68bf75b0a
youpiyoful <yoanfornari@gmail.com>
2020-11-12 12:01:29 +0100

    :construction: write sql for groups_user

---
+++ groups_user_create.sql
@@ -0,0 +1,4 @@
+insert into public.user_groups (user_groups_name) values ('pizzaïolo');
+insert into public.user_groups (user_groups_name) values ('client');
+insert into public.user_groups (user_groups_name) values ('manager');
+insert into public.user_groups (user_groups_name) values ('livreur');
