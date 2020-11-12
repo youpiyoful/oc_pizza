bbfdfc4f81eba985e474c576c81b0acabf577df5
youpiyoful <yoanfornari@gmail.com>
2020-11-12 15:43:40 +0100

    :sparkles: create insertion for address table

---
+++ insert_address.sql
@@ -0,0 +1,10 @@
+insert into public.address (city, street, postal_code) values ('Bernard', 'chemin Isaac Thibault', 32199);
+insert into public.address (city, street, postal_code) values ('Faure', '559, chemin Laurence Dubois', 56946);
+insert into public.address (city, street, postal_code) values ('Saint Julien', 'rue de Lamy', 11982);
+insert into public.address (city, street, postal_code) values ('Sainte Constance', '53, rue Marianne Boucher', 55303);
+insert into public.address (city, street, postal_code) values ('Meunier', '19, rue Begue', 72835);
+insert into public.address (city, street, postal_code) values ('Gimenez', '42, rue Teixeira', 34675);
+insert into public.address (city, street, postal_code) values ('Saint Pauline-les-Bains', '820, boulevard Roche', 68125);
+insert into public.address (city, street, postal_code) values ('CoulonVille', '78, chemin de Humbert', 79635);
+insert into public.address (city, street, postal_code) values ('Blin', '38, avenue Christine Monnier', 12322);
+insert into public.address (city, street, postal_code) values ('Chevallier', '87, boulevard de Nicolas', 73374);
