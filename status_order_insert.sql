0d4c760f25c6d5c4d7ed8ea64f92b5f10b5f1949
youpiyoful <yoanfornari@gmail.com>
2020-11-13 21:01:38 +0100

    :construction: insert status_order for an other client and command

--- status_order_insert.sql
+++ status_order_insert.sql
@@ -2,7 +2,7 @@ insert into public.status_order (id_order, id_status, "time", is_active, id_user
 insert into public.status_order (id_order, id_status, "time", is_active, id_user) values (4, 3, '2020-11-13 19:32:10.568816', false, 9);
 insert into public.status_order (id_order, id_status, "time", is_active, id_user) values (4, 4, '2020-11-13 19:42:10.568816', false, 9);
 insert into public.status_order (id_order, id_status, "time", is_active, id_user) values (4, 5, '2020-11-13 19:52:10.568816', true, 9);
-insert into public.status_order (id_order, id_status, "time", is_active, id_user) values (5, 8);
+insert into public.status_order (id_order, id_status, "time", is_active, id_user) values (5, 1, '2020-11-13 19:22:10.568816', true, 8);
 
 
 -- pour calculer le délais d'attente de la préparation a la livraison d'un produit, il faut faire la différence entre la colomne time de la ligne possédant l'id_status 1 et celle de la ligne possédant l'id_status 5
