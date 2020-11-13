e93e50531727bbb7061ef0cdbb7b4b950ff751dc
youpiyoful <yoanfornari@gmail.com>
2020-11-13 17:20:49 +0100

    :sparkles: insert data in stock with id_product and id_pizzeria

---
+++ stock_insert.sql
@@ -0,0 +1,23 @@
+-- pizzeria paris
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (13, 2, '2020/12/16', '2020/12/23', 200);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (14, 2, '2020/12/16', '2020/12/23', 1000);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (15, 2, '2020/12/16', '2020/12/23', 500);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (16, 2, '2020/12/16', '2020/12/23', 150);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (17, 2, '2020/12/16', '2020/12/23', 300);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (18, 2, '2020/12/16', '2020/12/23', 500);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (19, 2, '2020/12/16', '2020/12/23', 300);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (20, 2, '2020/12/16', '2020/12/23', 300);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (21, 2, '2020/12/16', '2020/12/23', 300);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (22, 2, '2020/12/16', '2020/12/23', 300);
+
+-- pizzeria besancon
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (13, 1, '2020/12/13', '2020/12/20', 200);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (14, 1, '2020/12/13', '2020/12/20', 1000);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (15, 1, '2020/12/13', '2020/12/20', 500);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (16, 1, '2020/12/13', '2020/12/20', 150);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (17, 1, '2020/12/13', '2020/12/20', 300);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (18, 1, '2020/12/13', '2020/12/20', 500);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (19, 1, '2020/12/13', '2020/12/20', 300);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (20, 1, '2020/12/13', '2020/12/20', 300);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (21, 1, '2020/12/13', '2020/12/20', 300);
+insert into public.stock(id_product, id_pizzeria, last_replineshment, next_replenishment, quantity) values (22, 1, '2020/12/13', '2020/12/20', 300);
