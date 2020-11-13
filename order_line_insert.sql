e70f8e21824478e788cfe6a46c740c9f4b18edf4
youpiyoful <yoanfornari@gmail.com>
2020-11-13 20:36:42 +0100

    :sparkles: insert multi line for many commands

---
+++ order_line_insert.sql
@@ -0,0 +1,3 @@
+insert into public.order_line (id_order, id_product, product_price, product_quantity) values (4, 3, 11, 1);
+insert into public.order_line (id_order, id_product, product_price, product_quantity) values (4, 5, 14, 1);
+insert into public.order_line (id_order, id_product, product_price, product_quantity) values (5, 9, 15, 5);
