b5029f6a063dcb076da5588b57cfb57cccb1fdad
youpiyoful <yoanfornari@gmail.com>
2020-11-13 11:08:54 +0100

    :sparkles: create link btw product_pizza and product_ingredient from product table into composition

---
+++ composition_insert.sql
@@ -0,0 +1,80 @@
+-- pizza_Chauvin-la-Foret --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (3, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (3, 14, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (3, 15, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (3, 17, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (3, 20, 1);
+
+-- pizza_Munoz-sur-Evrard --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (4, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (4, 16, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (4, 17, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (4, 18, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (4, 19, 1);
+
+-- pizza_Michaud --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (5, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (5, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (5, 14, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (5, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (5, 13, 1);
+
+-- pizza_Sainte Matthieu-sur-Mer --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (6, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (6, 22, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (6, 14, 2);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (6, 20, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (6, 21, 1);
+
+-- pizza_BaillyBourg --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (7, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (7, 14, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (7, 15, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (7, 16, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (7, 17, 1);
+
+--pizza_Launay--
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (8, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (8, 20, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (8, 14, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (8, 16, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (8, 21, 1);
+
+-- pizza_Payet --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (9, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (9, 16, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (9, 14, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (9, 18, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (9, 17, 1);
+
+-- pizza_Sainte HortenseBourg --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (10, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (10, 16, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (10, 14, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (10, 18, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (10, 20, 1);
+
+-- pizza_HamonBourg --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (11, 13, 2);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (11, 16, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (11, 19, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (11, 20, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (11, 21, 1);
+
+-- pizza_Le Roux-sur-Monnier --
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (12, 13, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (12, 16, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (12, 17, 2);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (12, 18, 1);
+INSERT INTO public.composition (id_ingredient, id_product, quantity_ingredient) VALUES (12, 22, 1);
+
+-- 13 * 13
+-- 7 * 14
+-- 2 * 15
+-- 7 * 16
+-- 5 * 17
+-- 4 * 18
+-- 2 * 19
+-- 5 * 20
+-- 3 * 21
+-- 2 * 22
