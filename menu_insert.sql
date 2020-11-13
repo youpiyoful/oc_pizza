869a9e621dea35bf28fec3f8753e7d82e5afa29e
youpiyoful <yoanfornari@gmail.com>
2020-11-13 16:30:49 +0100

    :sparkles: insert menu for the pizza product and the 2 pizzeria

---
+++ menu_insert.sql
@@ -0,0 +1,24 @@
+-- menu pizzéria Paris
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 3, 10.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 5, 13.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 6, 14.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 7, 12.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 8, 10.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 9, 14.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 10, 11.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 11, 11.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 12, 11.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (2, 13, 11.95);
+
+
+-- menu pizzéria Besançon
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 3, 10.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 4, 12.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 5, 13.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 6, 14.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 7, 12.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 8, 10.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 9, 14.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 11, 11.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 12, 11.95);
+insert into public.menu (id_pizzeria, id_product, menu_price) values (1, 13, 11.95);
