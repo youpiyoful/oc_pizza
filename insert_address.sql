b451d432cbecd53a3a1daf99885e0ab72822697b
youpiyoful <yoanfornari@gmail.com>
2020-11-13 11:19:28 +0100

    :hammer: add address for pizzeria

--- insert_address.sql
+++ insert_address.sql
@@ -1,3 +1,4 @@
+-- user address
 insert into public.address (city, street, postal_code) values ('Bernard', 'chemin Isaac Thibault', 32199);
 insert into public.address (city, street, postal_code) values ('Faure', '559, chemin Laurence Dubois', 56946);
 insert into public.address (city, street, postal_code) values ('Saint Julien', 'rue de Lamy', 11982);
@@ -8,3 +9,9 @@ insert into public.address (city, street, postal_code) values ('Saint Pauline-le
 insert into public.address (city, street, postal_code) values ('CoulonVille', '78, chemin de Humbert', 79635);
 insert into public.address (city, street, postal_code) values ('Blin', '38, avenue Christine Monnier', 12322);
 insert into public.address (city, street, postal_code) values ('Chevallier', '87, boulevard de Nicolas', 73374);
+
+-- pizzeria address
+insert into public.address (city, street, postal_code) values ('Besancon', '132 rue de Belfort', 25000);
+insert into public.address (city, street, postal_code) values ('Paris', '54 rue Montmartre', 75018);
+
+
